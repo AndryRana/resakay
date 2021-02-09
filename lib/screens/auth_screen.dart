105cf3653811aa746cd208e72113f853a12718ef
AndryRana <lsourcus@gmail.com>
2021-02-10 00:30:33 +0100

    Firebase, Image Upload, Push Notifications - Building a Chat App - 313 -  Flutter & the Firebase SDK - 314. Adding *GoogleServices-Info.plist* correctly to iOS - 315. Rendering Stream Data with StreamBuilder -316. Adding Data - 317. A Basic Authentication Screen - 318. Adding an App Theme - 319. Creating an Authentication Form - 320. Connecting Auth Form & Auth Screen -321. Adding the firebase_auth Packag -322. Implementing Authentication

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+
+import 'package:resakay/widgets/auth/auth_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String userName,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } on PlatformException catch (e) {
+      var message = 'An error occured, Please check yput credentials!';
+
+      if (e.message != null) {
+        message = e.message;
+      }
+
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(_submitAuthForm),
+    );
+  }
+}
